d3f454ac14e88f3e3a49a793820372b508feae4a
Raghav <ragsaxen@cisco.com>
2022-06-28 08:51:52 +0530

    List View

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'location_detail.dart';
 import 'models/location.dart';
 import 'mocks/mock_location.dart';
+import 'location_list.dart';
 
 void main() {
-  final Location locaton = MockLocation.FetchAny();
-  runApp(MaterialApp(home: LocationDetails(locaton)));
+  final mocklocations = MockLocation.fetchAll();
+  runApp(MaterialApp(home: LocationList(mocklocations)));
 }
